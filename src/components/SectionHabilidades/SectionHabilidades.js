2d3b4d1944d6eed1397b962a76d0d70a873a5fb6
Pol Gubau <gubaupol@gmail.com>
2022-09-15 21:51:33 +0200

    Create SectionHabilidades.js

---
+++ src/components/SectionHabilidades/SectionHabilidades.js
@@ -0,0 +1,31 @@
+import React from "react";
+import TitulosSeccion from "../TitulosSeccion/TitulosSeccion";
+import "./SectionHabilidades.css";
+import { motion } from "framer-motion";
+
+export default function SectionHabilidades({ titulo, habilidades }) {
+  return (
+    <div className="datosPersonales">
+      <TitulosSeccion titulo={titulo} />
+
+      <ul className="listaHabilidades">
+        {habilidades.map((habilidad, index) => (
+          <li key={index} className="habilidad">
+            {habilidad}
+
+            <div className="barra">
+              {/* Apply a randpm with every porcentaje */}
+
+              <motion.div
+                className="porcentaje"
+                initial={{ width: 0 }}
+                animate={{ width: `${Math.random() * 100}%` }}
+                transition={{ type: "spring", stiffness: 100 }}
+              ></motion.div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
