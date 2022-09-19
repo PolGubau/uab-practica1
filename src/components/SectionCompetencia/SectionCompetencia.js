d9f64fa0009a7c4cb8997776a4fc6248e4a1eee5
Pol Gubau <gubaupol@gmail.com>
2022-09-19 16:47:58 +0200

    Update SectionCompetencia.js

--- src/components/SectionCompetencia/SectionCompetencia.js
+++ src/components/SectionCompetencia/SectionCompetencia.js
@@ -1,13 +1,20 @@
 import React from "react";
 import TitulosSeccion from "../TitulosSeccion/TitulosSeccion";
 import "./SectionCompetencia.css";
+
+import { IoMdArrowDropright } from 'react-icons/io';
+
+
+
 export default function SectionCompetencia() {
   return (
     <>
       <TitulosSeccion titulo={"Competencias"} />
-      <div className="text">
-        <p>Comunicación</p>
-        <p>Trabajo en equipo</p>
+      <div className="textCompetencia">
+        <p className="competencia"><IoMdArrowDropright className="iconCompetencia" size={20}/>Comunicación</p>
+        <p className="competencia">
+        <IoMdArrowDropright className="iconCompetencia" size={20}/>
+          Trabajo en equipo</p>
       </div>
     </>
   );
